34f8dc53fd5b35fcf48de12575b38b815a4a405b
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-25 18:33:31 +0530

    Basic

---
+++ Basic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mohan: Hi Krishna, How are you?"
+echo "Krishna: Hi Mohan, I'm fine How are you?"
+echo "Mohan: I'm good Krishna, thank you"
+echo "Krishna: That's great Mohan, thank you"
