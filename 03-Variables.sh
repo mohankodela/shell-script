84b6a68699e5905a0ce28b69302e06491cc15b59
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-25 18:38:13 +0530

    Variables

---
+++ 03-Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Using Variables
+
+PERSON1=Mohan
+PERSON2=Krishna
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I'm fine How are you?"
+echo "$PERSON1: I'm good $PERSON2, thank you"
+echo "$PERSON2: That's great $PERSON1, thank you"
