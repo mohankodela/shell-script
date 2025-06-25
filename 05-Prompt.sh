2c2111bd14e8bfbdaa9e6f63e38d61f375370266
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-25 20:18:19 +0530

    Variables

---
+++ 05-Prompt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$1: Hi $2, How are you?"
+echo "$2: Hi $1, I'm fine How are you?"
+echo "$1: I'm good $2, thank you"
+echo "$2: That's great $1, thank you"
