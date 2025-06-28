e3dbce3b3f476d271700acd6293eed22c6c4a7c7
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-28 11:14:28 +0530

    variable

--- 08-Arrays.sh
+++ 08-Arrays.sh
@@ -4,6 +4,8 @@ FRUITS=("Apple" "Banana" "Carrot")
 
 echo "First Fruit is: ${FRUITS[0]}"
 
+echo "First Fruit is: ${FRUITS[1]}"
+
 echo "Arguments Passed: ${FRUITS[$@]}"
 
 echo "How Many Arguments Passed: $#"
