e96472c148cbaa9a7bcb337a2d059e45a0cc8eb1
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-28 09:52:25 +0530

    variable

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -3,8 +3,8 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 100 ]
-    then
-        echo "Entered Number $NUMBER is greater than 100"
-    else
-         echo "Entered Number $NUMBER is greater than 100"
+then
+    echo "Entered Number $NUMBER is greater than 100"
+else
+    echo "Entered Number $NUMBER is greater than 100"
 fi
