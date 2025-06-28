8357d1c43c254e684f2f98359b2018c71e30c16a
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-28 09:53:18 +0530

    variable

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -6,5 +6,5 @@ if [ $NUMBER -gt 100 ]
 then
     echo "Entered Number $NUMBER is greater than 100"
 else
-    echo "Entered Number $NUMBER is greater than 100"
+    echo "Entered Number $NUMBER is not greater than 100"
 fi
