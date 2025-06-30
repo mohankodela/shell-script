e9c99bfca59bb23011d6c068fbb7d030782f8a10
mohankodela <kodelamohankrishna999@gmail.com>
2025-06-30 16:57:31 +0530

    Package

--- 12-package.sh
+++ 12-package.sh
@@ -6,6 +6,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H:%M:%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R Your are not root user $N"
