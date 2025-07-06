a45e1e2d7eee7023240020d21c1ac555bcd8d63c
mohankodela <kodelamohankrishna999@gmail.com>
2025-07-06 10:27:56 +0530

    log

--- log-rotate.sh
+++ log-rotate.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-SOURCE_DIR="/tmp/shell-logs"
+SOURCE_DIR="/tmp/shellscript-logs"
 
-if [ ! -d $SORCE_DIR ]
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
 then
-    echo -e "Source directory $SORCE_DIR doesn't exists"
+    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
 fi
 
-FILES_TO_DELETE=$(find $SORCE_DIR -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
     echo "Deleting file: $line"
+    rm -rf $line
 done <<< $FILES_TO_DELETE
